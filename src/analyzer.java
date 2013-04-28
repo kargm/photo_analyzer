f8e35cf35361e49eddb049ed0b34001b2b32ec21
Michael Karg <kargm@in.tum.de>
2013-04-28 18:37:22 +0200

    Moved string conversions to utils

--- src/analyzer.java
+++ src/analyzer.java
@@ -17,7 +17,7 @@ public class analyzer {
 	private void getStatistics(File[] files) throws IOException {
 		try{
 	    for (File file : files) {
-	        if (!file.getName().equals(".DS_Store")) { //.DS_Store file causes problems TODO: Find solution for more problem-files
+	        if (!file.getName().equals(".DS_Store")) { //.DS_Store file causes problems TODO: Find general solution for more problem-files
 	    	if (file.isDirectory()) {
 	            //.out.println("Directory: " + file.getName());
 	            getStatistics(file.listFiles()); // Calls same method again.
@@ -50,40 +50,14 @@ public class analyzer {
 	public HashMap<String, Integer> analyseDates(ExifSubIFDDirectory dir, 
 			HashMap<String, Integer> days_map, 
 			HashMap<String, Integer> months_map) {
-		
-		String[] weekday = new String[8];
-				weekday[0]="Unknown";
-				weekday[1]="Sunday";
-				weekday[2]="Monday";
-				weekday[3]="Tuesday";
-				weekday[4]="Wednesday";
-				weekday[5]="Thursday";
-				weekday[6]="Friday";
-				weekday[7]="Saturday";
-				
-		String[] month_string = new String[13];
-			month_string[12] = "Unknown";
-			month_string[0] = "January";
-			month_string[1] = "February";
-			month_string[2] = "March";
-			month_string[3] = "April";
-			month_string[4] = "May";
-			month_string[5] = "June";
-			month_string[6] = "July";
-			month_string[7] = "August";
-			month_string[8] = "September";
-			month_string[9] = "October";
-			month_string[10] = "November";
-			month_string[11] = "Dezember";
-			
 				
 				// query the tag's value
 				try {
 					java.util.Date date = dir.getDate(ExifSubIFDDirectory.TAG_DATETIME_ORIGINAL);
 					Calendar cal = Calendar.getInstance();
 					cal.setTime(date);
-					String days_key = weekday[cal.get(Calendar.DAY_OF_WEEK)].toString();
-					String month_key = month_string[cal.get(Calendar.MONTH)].toString();
+					String days_key = utils.get_weekday_string(cal.get(Calendar.DAY_OF_WEEK));
+					String month_key = utils.get_month_string(cal.get(Calendar.MONTH)).toString();
 					
 					// Weekdays
 					if (days_map.containsKey(days_key)) {
